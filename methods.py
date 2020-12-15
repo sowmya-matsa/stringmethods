30ec19dddc7557d7d1b4890c91ae6db30ebb8fd6
sowmya.matsa <sowmya.matsa@gmail.com>
2020-12-15 18:08:37 +0530

    first commit

---
+++ methods.py
@@ -0,0 +1,9 @@
+str = "sowmya"
+str1 = "intern"
+str2 = "I like kids "
+print(str)
+print(str.upper())
+print(str1.lower())
+print(str1.count("n"))
+print(str2.casefold())
+
---
+++ string.py
@@ -0,0 +1,7 @@
+no = 1
+no1 = 2
+result = no + no1
+print("result:", result)
+print(no)
+
+
